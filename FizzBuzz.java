c56b74b5b86da3850e68a280eabaa2f78d8c5ebe
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2024-08-25 17:28:46 +0530

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,15 @@
+public class FizzBuzz {
+    public static void fizzBuzz(int n) {
+        // Your code here
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(16);
+    }
+}
+
+// Write a method fizzBuzz(int n) that prints the 
+// numbers from 1 to n. But for multiples of three, 
+// print "Fizz" instead of the number, and for the 
+// multiples of five, print "Buzz". For numbers which 
+// are multiples of both three and five, print "FizzBuzz".
